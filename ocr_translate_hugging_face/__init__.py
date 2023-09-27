ba87f6b2490e042dae509cf1b79d5828f464e1b6
Crivella <davidecrivella@yahoo.it>
2023-09-27 16:42:14 +0200

    Bump version number

--- ocr_translate_hugging_face/__init__.py
+++ ocr_translate_hugging_face/__init__.py
@@ -18,7 +18,7 @@
 ###################################################################################
 """Plugins to enable usage of HuggingFace Models in ocr_translate"""
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 khawhite_ocr_model_data = {
     'name': 'kha-white/manga-ocr-base',
